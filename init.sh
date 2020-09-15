337204c01ff68b5bd1134979f1c8d61bccbad8d5
Сандро <warsan00@mail.ru>
2020-09-15 08:50:41 +0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,12 +6,11 @@ HOST="$(cut -d'-' -f3 <<<"$s")"
 
 if [ $HOST != '0rfu5' ] && [ ! -f data/ghost-local.db ]; then
 (
-  node -p "JSON.stringify({...require('./base-config.development.json'), 
-  url: \"https://$HOST.sse.codesandbox.io/\"}, null, 2)" > config.development.json
+  node -p "JSON.stringify({...require('./base-config.development.json'), url: \"https://$HOST.sse.codesandbox.io/\"}, null, 2)" > config.development.json
   mkdir /sandbox/logs
   mkdir /sandbox/data  
-  cd node_modules/ghost
+  cd node_modules/blog-prizrak
   knex-migrator init
-  mv /sandbox/node_modules/ghost/content/data/ghost-dev.db/sandbox/data/ghost-local.db
+  mv /sandbox/node_modules/blog-prizrak/content/data/ghost-dev.db /sandbox/data/ghost-local.db
 )
 fi
